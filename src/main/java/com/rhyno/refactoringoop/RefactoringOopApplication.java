20f36ac1a464cfd9b94cf1b1639c023868fb4a4c
pqzmggg <903!QNFrhrl>
2021-04-30 11:19:34 +0900

    First commit for project

---
+++ src/main/java/com/rhyno/refactoringoop/RefactoringOopApplication.java
@@ -0,0 +1,13 @@
+package com.rhyno.refactoringoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefactoringOopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefactoringOopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rhyno/refactoringoop/RefactoringOopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhyno.refactoringoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefactoringOopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
